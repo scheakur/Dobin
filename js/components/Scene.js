b355ad193eafd0b709da3f00caa07919fa643872
scheakur <scheakur@gmail.com>
2016-07-02 17:00:53 +0900

    Fix lint

--- js/components/Scene.js
+++ js/components/Scene.js
@@ -1,7 +1,4 @@
-import React, {
-  Component,
-  PropTypes,
-} from 'react';
+import React, { PropTypes } from 'react';
 
 import {
   StatusBar,
@@ -12,36 +9,6 @@ import {
 import { Header } from './';
 
 import { STATUS_BAR_STYLE, HEADER_HEIGHT } from '../const';
-
-export default class Scene extends Component {
-
-  render() {
-    const { title, leftItem, rightItem, style } = this.props;
-
-    return (
-      <View style={[styles.container, style]}>
-        <StatusBar barStyle={STATUS_BAR_STYLE}/>
-        <Header title={title} leftItem={leftItem} rightItem={rightItem}/>
-        <View style={styles.contents}>
-          {this.props.children}
-        </View>
-      </View>
-    );
-  }
-
-}
-
-
-Scene.propTypes = {
-  title: PropTypes.string,
-  leftItem: PropTypes.object,
-  rightItem: PropTypes.object,
-};
-
-
-Scene.defaultProps = {
-  title: 'Dummy Title',
-};
 
 
 const styles = StyleSheet.create({
@@ -58,3 +25,35 @@ const styles = StyleSheet.create({
     color: '#f00',
   },
 });
+
+
+const Scene = props => {
+  const { title, leftItem, rightItem, style } = props;
+
+  return (
+    <View style={[styles.container, style]}>
+      <StatusBar barStyle={STATUS_BAR_STYLE} />
+      <Header title={title} leftItem={leftItem} rightItem={rightItem} />
+      <View style={styles.contents}>
+        {props.children}
+      </View>
+    </View>
+  );
+};
+
+
+Scene.propTypes = {
+  title: PropTypes.string,
+  leftItem: PropTypes.object,
+  rightItem: PropTypes.object,
+  style: PropTypes.object,
+  children: PropTypes.array,
+};
+
+
+Scene.defaultProps = {
+  title: 'Dummy Title',
+};
+
+
+export default Scene;
