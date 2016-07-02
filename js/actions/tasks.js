0f5a914736bc6d3af68123a983429d4c7cb187d7
scheakur <scheakur@gmail.com>
2016-07-02 16:57:41 +0900

    Fix lint

--- js/actions/tasks.js
+++ js/actions/tasks.js
@@ -3,9 +3,7 @@ import { createAction } from 'redux-actions';
 const addTaskStart = createAction('add task: start');
 const addTaskEnd = createAction('add task: end');
 
-export const addTask = (task) => {
-  return (dispatch) => {
-    dispatch(addTaskStart(task));
-    dispatch(addTaskEnd(task));
-  };
+export const addTask = task => dispatch => {
+  dispatch(addTaskStart(task));
+  dispatch(addTaskEnd(task));
 };
