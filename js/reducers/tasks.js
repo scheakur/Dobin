ff97503f952cc7596b8bc8f40f52e6e45919be05
scheakur <scheakur@gmail.com>
2016-07-10 18:59:09 +0900

    Increase dummy data

--- js/reducers/tasks.js
+++ js/reducers/tasks.js
@@ -8,6 +8,18 @@ const initialState = {
     { title: 'Taks 001  001 001 001 001 001 001' },
     { title: 'Taks 002  002 002 002 002 002 002 002 002 002 002 002' },
     { title: 'Taks 003  003 003 003 003 003 003 003 003 003 003 003 003 003 003 003' },
+    { title: 'Taks 004' },
+    { title: 'Taks 005' },
+    { title: 'Taks 006' },
+    { title: 'Taks 007' },
+    { title: 'Taks 008' },
+    { title: 'Taks 009' },
+    { title: 'Taks 010' },
+    { title: 'Taks 011' },
+    { title: 'Taks 012' },
+    { title: 'Taks 013' },
+    { title: 'Taks 014' },
+    { title: 'Taks 015' },
   ],
 };
 
