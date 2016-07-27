67a189cca29323abf52eb6d4d71fb3b8799bd2c8
scheakur <scheakur@gmail.com>
2016-07-27 22:34:48 +0900

    Update dummy menu

--- js/components/Menu.js
+++ js/components/Menu.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { STATUS_BAR_HEIGHT } from '../const';
 
@@ -14,25 +17,77 @@ const styles = StyleSheet.create({
     backgroundColor: '#263238',
     paddingTop: STATUS_BAR_HEIGHT,
   },
+  menu: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 5,
+  },
+  icon: {
+    fontSize: 16,
+    color: '#fff',
+    marginRight: 5,
+  },
   text: {
+    fontSize: 16,
     color: '#fff',
   },
 });
 
 
+
+
 export default class Menu extends Component {
 
-  renderMenu() {
+  constructor(props) {
+    super(props);
+    this.renderMenu = this.renderMenu.bind(this);
+  }
+
+
+  getMenuList() {
+    return [
+      {
+        label: 'Tasks',
+        icon: 'playlist-add-check',
+        onPress: () => {},
+      },
+      {
+        label: 'History',
+        icon: 'history',
+        onPress: () => {},
+      },
+      {
+        label: 'Settings',
+        icon: 'settings',
+        onPress: () => {},
+      },
+    ];
+  }
+
+
+  renderMenu(menu, index) {
     return (
-      <Text style={styles.text}>menu</Text>
+      <TouchableOpacity
+        key={`menu-${index}`}
+        style={styles.menu}
+        onPress={menu.onPress}
+      >
+        <Icon style={styles.icon} name={menu.icon} />
+        <Text style={styles.text}>{menu.label}</Text>
+      </TouchableOpacity>
     );
+  }
+
+
+  renderMenuList() {
+    return this.getMenuList().map(this.renderMenu);
   }
 
 
   render() {
     return (
       <View style={styles.container}>
-        {this.renderMenu()}
+        {this.renderMenuList()}
       </View>
     );
   }
