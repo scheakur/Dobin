b231b743b90bc37c35636714e9bca3458930855e
scheakur <scheakur@gmail.com>
2016-08-02 18:19:52 +0900

    Fix reference to removed component

--- js/scenes/index.js
+++ js/scenes/index.js
@@ -1,12 +1,10 @@
 import Home from './Home';
-import TaskForm from './TaskForm';
 import TaskList from './TaskList';
 import TaskTimer from './TaskTimer';
 import Settings from './Settings';
 
 export {
   Home,
-  TaskForm,
   TaskList,
   TaskTimer,
   Settings,
